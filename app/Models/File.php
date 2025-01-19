a645f8a1e37ce348990b2c8a2a9edd6b494fc27a
AbdullahRahmoun1 <aboodyggkol@gmail.com>
2025-01-19 23:48:50 +0300

    Small Changes.

--- app/Models/File.php
+++ app/Models/File.php
@@ -6,6 +6,7 @@ use App\Enums\GroupFileStatusEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Str;
 use Wever\Laradot\App\Traits\HasFiles;
 
 class File extends Model
